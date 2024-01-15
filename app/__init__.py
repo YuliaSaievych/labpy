df8b037dcdc3c3ade6bab82ff50b1801d7b7caa4
YuliaSaievych <yuliia.saievych.21@pnu.edu.ua>
2024-01-15 15:42:56 +0200

    lab6

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,14 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
 
 from app import views
+
