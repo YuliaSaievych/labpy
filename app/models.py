d42cc1e13525f989322fd25412dc9862100811d5
YuliaSaievych <yuliia.saievych.21@pnu.edu.ua>
2024-01-20 21:32:42 +0200

    lab7

--- app/models.py
+++ app/models.py
@@ -1,3 +1,6 @@
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
+
 from app import db
 
 class ToDo(db.Model):
@@ -7,12 +10,21 @@ class ToDo(db.Model):
     done = db.Column(db.Boolean, default=False)
     status = db.Column(db.String(25), default="IN_PROGRESS")
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(255), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
+    password_hash = db.Column(db.String(128))
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email} ')"
