606a2067bd4468570b7af732c90bc1d7fe50ed91
YuliaSaievych <yuliia.saievych.21@pnu.edu.ua>
2024-01-22 20:23:31 +0200

    lab8

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,15 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
+from config import UPLOAD_FOLDER
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SECRET_KEY'] = 'asdasd'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# app.config['ALLOWED_EXTENSIONS'] = ALLOWED_EXTENSIONS
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
--- config.py
+++ config.py
@@ -5,5 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 WTF_CSRF_ENABLED = True
 SECRET_KEY = '<KEY>'
 
+UPLOAD_FOLDER = 'sqlite:///db.sqlite'
+
 SQLALCHEMY_DATABASE_URI = 'instance' + os.path.join(basedir, 'db.sqlite')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
